407577aead6fd1dee8e942abe65e1fe950b5d021
Echidna <176174870+EchidnaTheG@users.noreply.github.com>
2025-02-27 00:50:49 -0400

    Initial node logic done, first test made

--- src/textnode.py
+++ src/textnode.py
@@ -9,14 +9,13 @@ class TextType(Enum):
     IMAGE = "image"
 
 class TextNode():
-    def __init__(self, TEXT, TEXT_TYPE, URL):
+    def __init__(self, TEXT, TEXT_TYPE, URL=None):
         self.text= TEXT
         self.text_type=TEXT_TYPE
         self.url= URL
-    def __eg__(self,other_node):
+    def __eq__(self,other_node):
         if self.text == other_node.text and self.text_type == other_node.text_type and self.url == other_node.url:
             return True
         
-    
-
-    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
