9943d66fc148b4f9a4c9ea523b594898e66dee72
Echidna <176174870+EchidnaTheG@users.noreply.github.com>
2025-02-28 14:56:14 -0400

    Created Initial Parent Node Logic, will work on testing now and finalizing it

--- src/parentnode.py
+++ src/parentnode.py
@@ -1,5 +1,18 @@
 from htmlnode import HTMLNode
 
 class ParentNode(HTMLNode):
-    #will continue later, have to do calc hw
-    pass
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None , children, props)
+    
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Missing Tag")
+        if self.children is None:
+            raise ValueError("Missing Children")
+        html= f"<{self.tag}>"
+        for child in self.children:
+            html += child.to_html()
+
+        html += f"</{self.tag}>"
+        return html
+       
