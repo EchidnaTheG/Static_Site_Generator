7b41699178b983736f55d006e31ab925656438bc
Echidna <176174870+EchidnaTheG@users.noreply.github.com>
2025-02-28 18:44:17 -0400

    Added Unit tests for text node, focusing on textnode to html node conversion

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from leafnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -16,7 +17,25 @@ class TextNode():
     def __eq__(self,other_node):
         if self.text == other_node.text and self.text_type == other_node.text_type and self.url == other_node.url:
             return True
+        else:
+            return False
         
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
     
+    def text_node_to_html_node(self):
+        if self.text_type not in TextType:
+            raise ValueError(f"Invalid Text Type, {self.text_type} isn't supported")
+        if self.text_type == TextType.TEXT:
+            return LeafNode(None,self.text)
+        elif self.text_type == TextType.BOLD:
+            return LeafNode("b",self.text)
+        elif self.text_type == TextType.ITALIC:
+            return LeafNode("i", self.text)
+        elif self.text_type == TextType.CODE:
+            return LeafNode("code", self.text)
+        elif self.text_type == TextType.LINK:
+            return LeafNode("a", self.text,props={"href":self.url})
+        elif self.text_type == TextType.IMAGE:
+            return LeafNode("img", None,props={"src":self.url, "alt":self.text})
+        
