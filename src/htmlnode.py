a53448f3e4deab0655e3ea417d0388724b956bd7
Echidna <ej.cruz.sant@gmail.com>
2025-02-27 22:38:54 -0400

    Added initial tests, might have some future problems regarding how I implemented spaces for the props attriubute

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,14 +11,16 @@ class HTMLNode:
         raise NotImplementedError("Error!")
 
     def props_to_html(self):
-        if self.props == None:
+        if self.props is None:
             return ""
-        string_representation=""
-        for key, value in self.props.items():
-            string_representation += " " + key + "=" + fr'"{value}"' + " "
-        print(string_representation) 
-        return string_representation
-    
-x= HTMLNode(props={"href": "https://www.google.com","target": "_blank",})
-x.props_to_html()
-print(x)
+        props_strings = [f'{key}="{value}"' for key, value in self.props.items()]
+        return f" {' '.join(props_strings)}"
+  
+    def __eq__(self, other):
+        if not isinstance(other, HTMLNode):
+            return False
+        return(self.tag == other.tag and 
+                self.value == other.value and 
+                self.children == other.children and 
+                self.props == other.props)
+        
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -4,29 +4,29 @@ from htmlnode import HTMLNode
 
 
 class TestHTMLNode(unittest.TestCase):
-    def test_eq(self):
-        node = TextNode("This is a text node", TextType.BOLD)
-        node2 = TextNode("This is a text node", TextType.BOLD)
+    def test_eq_props(self):
+        node = HTMLNode(props={"href": "https://www.google.com","target": "_blank",})
+        node2 = HTMLNode(props={"href": "https://www.google.com","target": "_blank",})
         self.assertEqual(node, node2)
     
-    def test_noteqtype(self):
-        node = TextNode("This is a text node", TextType.BOLD)
-        node2 = TextNode("This is a text node", TextType.ITALIC)
+    def test_not_eq_props(self):
+        node = HTMLNode(props={"href": "https://www.google.com","target": "_blank",})
+        node2 = HTMLNode(props={"href": "https://www.google.com","target": "blank",})
         self.assertNotEqual(node, node2)
 
     def test_eqtext(self):
-        node = TextNode("This is a text node", TextType.BOLD)
-        node2 = TextNode("This is a text node", TextType.ITALIC)
-        self.assertEqual(node.text, node2.text)
-        
+        node = HTMLNode("h1","Welcome to My Website!", props={"href": "https://www.google.com","target": "_blank",})
+        node2 = HTMLNode("h1","WELcome to My Website!", props={"href": "https://www.google.com","target": "_blank",})
+        self.assertNotEqual(node.value, node2.value)
+
     def test_different_url(self):
-        node = TextNode("This is a text node", TextType.BOLD, "https://example.com")
-        node2 = TextNode("This is a text node", TextType.BOLD, "https://different.com")
+        node = HTMLNode(props={"href": "https://www.google.com","target": "_blank",})
+        node2 = HTMLNode(props={"href": "https://www.boot.dev/lessons/","target": "_blank",})
         self.assertNotEqual(node, node2)
 
     def test_ulrnone(self):
-        node = TextNode("This is a text node", TextType.BOLD)
-        self.assertEqual(node.url, None)
-
+        node = HTMLNode("h1","Welcome to My Website!")
+        self.assertEqual(node.props, None)
+    
 if __name__ == "__main__":
     unittest.main()
