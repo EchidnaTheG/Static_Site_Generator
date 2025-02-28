e438bdb4e32e2b0b22526b5590af04c05752e63e
Echidna <176174870+EchidnaTheG@users.noreply.github.com>
2025-02-27 20:08:55 -0400

    Started doing HTMLNode.py, working on making some initial unit tests for that too, for now copypasted preexisting tests for textnode.py, will modify later

---
+++ src/test_htmlnode.py
@@ -0,0 +1,32 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+    
+    def test_noteqtype(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_eqtext(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertEqual(node.text, node2.text)
+        
+    def test_different_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://example.com")
+        node2 = TextNode("This is a text node", TextType.BOLD, "https://different.com")
+        self.assertNotEqual(node, node2)
+
+    def test_ulrnone(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node.url, None)
+
+if __name__ == "__main__":
+    unittest.main()
