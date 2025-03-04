2478d5dc4307ab012b43124ba99b1721ac6817c8
Echidna <ej.cruz.sant@gmail.com>
2025-03-03 23:21:17 -0400

    Added final initial details to delimiter, will work on unit tests now

--- src/delimiter.py
+++ src/delimiter.py
@@ -6,9 +6,14 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         
         if old_node.text_type is not TextType.TEXT:
             new_nodes.append(old_node)
+            continue
 
+        if old_node.text.count(delimiter) % 2 != 0:
+            raise ValueError(f"Mismatched delimiters: {delimiter}")
+            
         parts = old_node.text.split(delimiter)
         for i, part in enumerate(parts):
+        
             if i % 2 == 0:  
                 if part:  
                     new_nodes.append(TextNode(part, TextType.TEXT))
@@ -20,5 +25,3 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 node = TextNode("This is text with a `code block` word", TextType.TEXT)
 new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
 print(new_nodes)    
-
-        
