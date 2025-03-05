8f1cd8afd7aa94aaeb74a938e58f4b58f6babc43
Echidna <ej.cruz.sant@gmail.com>
2025-03-05 00:24:14 -0400

    Added Regex for Markdown Pattern Matching URL and IMGs, also made tests for it

---
+++ src/markdown_extraction.py
@@ -0,0 +1,12 @@
+import re
+
+def extract_markdown_images(text):
+    regex_pattern_img= r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(regex_pattern_img, text)
+    return matches
+
+def extract_markdown_links(text):
+    regex_pattern_link= r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(regex_pattern_link, text)
+    return matches
+
