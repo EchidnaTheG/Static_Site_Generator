54a089757dae67c9900fbea3e9926e31ca5e6554
Echidna <ej.cruz.sant@gmail.com>
2025-03-06 20:44:52 -0400

    struggled a lot with the markdown stuff, had to use chatgpt, it passed all tests though...

--- src/markdown_extraction.py
+++ src/markdown_extraction.py
@@ -1,5 +1,5 @@
 import re
-
+from textnode import TextNode, TextType
 def extract_markdown_images(text):
     regex_pattern_img= r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
     matches = re.findall(regex_pattern_img, text)
@@ -11,5 +11,36 @@ def extract_markdown_links(text):
     return matches
 
 def split_nodes_image(old_nodes):
-    pass
+    new_nodes = []
+    for node in old_nodes:
+       if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+       if not node.text:
+           continue
+       images= extract_markdown_images(node.text)   
+       if not images:
+           new_nodes.append(node)
+           continue
+       
+       current_text= node.text
+       for alt_text, url in images:
+            image_markdown = f"![{alt_text}]({url})"
+            parts = current_text.split(image_markdown, 1)
+            if parts[0]:
+                new_nodes.append(TextNode(parts[0], TextType.TEXT))
+                
+            new_nodes.append(TextNode(alt_text, TextType.IMAGE, url))
+                
+            if len(parts) > 1:
+                current_text = parts[1]
+            else:
+                current_text = ""
+
+       if current_text:
+            new_nodes.append(TextNode(current_text, TextType.TEXT))
     
+    return new_nodes
+
+node =TextNode("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",TextType.TEXT)
+print(split_nodes_image([node]))
