3673755e66ec7ebe6618ec4a967f479f1483de73
Echidna <176174870+EchidnaTheG@users.noreply.github.com>
2025-03-06 20:44:52 -0400

    struggled a lot with the markdown stuff, had to use chatgpt, it passed all tests though...

--- src/test_markdown.py
+++ src/test_markdown.py
@@ -1,6 +1,7 @@
 import unittest
 
-from markdown_extraction import extract_markdown_links, extract_markdown_images
+from markdown_extraction import extract_markdown_links, extract_markdown_images, split_nodes_image
+from textnode import TextNode, TextType
 
 
 class TestMarkDown(unittest.TestCase):
@@ -70,6 +71,84 @@ class TestMarkDown(unittest.TestCase):
         """Test empty string input"""
         self.assertEqual(len(extract_markdown_links("")), 0)
         self.assertEqual(len(extract_markdown_images("")), 0)
+
+    def test_extract_single_image(self):
+        """Test extracting a single image from text"""
+        text = "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        matches = extract_markdown_images(text)
+        self.assertEqual(len(matches), 1)
+        self.assertEqual(matches[0], ("image", "https://i.imgur.com/zjjcJKZ.png"))
+
+    def test_extract_multiple_images(self):
+        """Test extracting multiple images"""
+        text = "![first](img1.png) and ![second](img2.jpg)"
+        matches = extract_markdown_images(text)
+        self.assertEqual(len(matches), 2)
+        self.assertEqual(matches[0], ("first", "img1.png"))
+        self.assertEqual(matches[1], ("second", "img2.jpg"))
+
+    def test_split_nodes_single_image(self):
+        """Test splitting nodes with a single image"""
+        node = TextNode("Before ![image](test.png) after", TextType.TEXT)
+        result = split_nodes_image([node])
+        self.assertEqual(len(result), 3)
+        self.assertEqual(result[0].text, "Before ")
+        self.assertEqual(result[1].text, "image")
+        self.assertEqual(result[1].url, "test.png")
+        self.assertEqual(result[2].text, " after")
+
+    def test_split_nodes_consecutive_images(self):
+        """Test handling consecutive images"""
+        node = TextNode("![one](1.png)![two](2.png)", TextType.TEXT)
+        result = split_nodes_image([node])
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[0].text, "one")
+        self.assertEqual(result[1].text, "two")
+
+    def test_split_nodes_with_non_text_nodes(self):
+        """Test that non-TEXT nodes are preserved"""
+        nodes = [
+            TextNode("![img](test.png)", TextType.TEXT),
+            TextNode("Bold", TextType.BOLD),
+            TextNode("![img2](test2.png)", TextType.TEXT)
+        ]
+        result = split_nodes_image(nodes)
+        self.assertEqual(len(result), 3)
+        self.assertEqual(result[1].text_type, TextType.BOLD)
+
+    def test_image_with_empty_alt_text(self):
+        """Test handling images with empty alt text"""
+        text = "![](empty.png)"
+        matches = extract_markdown_images(text)
+        self.assertEqual(matches[0], ("", "empty.png"))
+
+    def test_image_with_spaces(self):
+        """Test handling images with spaces in alt text and URL"""
+        text = "![Alt text with spaces](/path/to/image with spaces.jpg)"
+        matches = extract_markdown_images(text)
+        self.assertEqual(matches[0], ("Alt text with spaces", "/path/to/image with spaces.jpg"))
+
+    def test_invalid_image_syntax(self):
+        """Test handling invalid image markdown syntax"""
+        text = "![missing](] and ![missing too"
+        matches = extract_markdown_images(text)
+        self.assertEqual(len(matches), 0)
+
+    def test_links_vs_images(self):
+        """Test distinguishing between links and images"""
+        text = "[link](url.com) and ![image](img.png)"
+        image_matches = extract_markdown_images(text)
+        link_matches = extract_markdown_links(text)
+        self.assertEqual(len(image_matches), 1)
+        self.assertEqual(len(link_matches), 1)
+        self.assertEqual(image_matches[0][1], "img.png")
+        self.assertEqual(link_matches[0][1], "url.com")
+
+    def test_split_nodes_empty_text(self):
+        """Test splitting nodes with empty text"""
+        node = TextNode("", TextType.TEXT)
+        result = split_nodes_image([node])
+        self.assertEqual(len(result), 0)
 
   
 
